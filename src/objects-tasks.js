8dda04f799f4a70722d0d5d20507774f6cd2058a
Insspirit <kovshiktanya@gmail.com>
2025-03-11 00:42:02 +0300

    feat: first part of the tasks

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,9 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
+function shallowCopy(obj) {
+  const res = { ...obj };
+  return Object.assign(res);
 }
 
 /**
@@ -32,8 +33,22 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const arr = [...objects];
+  const obj = {};
+  arr.forEach((el) => {
+    if (typeof el === 'object') {
+      const entries = Object.entries(el);
+      entries.forEach(([key, v]) => {
+        if (!obj[key]) {
+          obj[key] = v;
+        } else {
+          obj[key] += v;
+        }
+      });
+    }
+  });
+  return obj;
 }
 
 /**
@@ -49,8 +64,12 @@ function mergeObjects(/* objects */) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const obj2 = { ...obj };
+  keys.forEach((el) => {
+    delete obj2[el];
+  });
+  return obj2;
 }
 
 /**
@@ -65,8 +84,9 @@ function removeProperties(/* obj, keys */) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  const res = JSON.stringify(obj1) === JSON.stringify(obj2);
+  return res;
 }
 
 /**
@@ -80,8 +100,10 @@ function compareObjects(/* obj1, obj2 */) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  let res = true;
+  if (Object.keys(obj).length > 0) res = false;
+  return res;
 }
 
 /**
